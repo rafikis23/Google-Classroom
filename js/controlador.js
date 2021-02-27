8eca5dfa1ccd7ef1f4a80065493a6424f51a2d76
rafikis23 <leocasco23@gmail.com>
2021-02-27 15:01:09 -0600

    first commit

---
+++ js/controlador.js
@@ -0,0 +1,394 @@
+// Aqui se va a generar el arreglo de JSON
+
+
+var classroom = [{
+    instructor: {
+        nombre: "Goku",
+        correo: "goku@gmail.com",
+        imagen: "goku.jpg",
+    },
+    clases: [{
+            nombreClase: "Programación II",
+            codigo: "CSD33XZ",
+            seccion: "1300",
+            descripcion: "Clase donde se enseña a programar",
+            participantes: [{
+                    nombre: "Krillin",
+                    correo: "krillin@gmail.com",
+                    imagen: "krillin.jpg",
+                },
+                {
+                    nombre: "Uub",
+                    correo: "Uub@gmail.com",
+                    imagen: "Uub.jpg"
+                },
+                {
+                    nombre: "Videl",
+                    correo: "Videl@gmail.com",
+                    imagen: "videl.jpg"
+                },
+                {
+                    nombre: "Bulma",
+                    correo: "bulma@gmail.com",
+                    imagen: "bulma.jpg"
+                }
+
+                // ...Otro participante
+            ],
+            anuncios: [{
+                    mensaje: "Hoy es el examen",
+                    fecha: "27/02/2020",
+                },
+                {
+                    mensaje: "Manana entrega de proyecto",
+                    fecha: "28/02/2021"
+                },
+                {
+                    mensaje: "Lunes ver wandavision",
+                    fecha: "01/03/2021"
+                }
+                // ...Otro anuncio
+            ],
+            asignaciones: [{
+                    titulo: "Tarea 1",
+                    descripcion: "Entregar un programa",
+                    fecha: "28/02/2020",
+                },
+                {
+                    titulo: "Tarea 2",
+                    descripcion: "Entregar un examen",
+                    fecha: "27/02/2021"
+                },
+                {
+                    titulo: "Tarea 3",
+                    descripcion: "Entregar proyecto",
+                    fecha: "28/02/2021"
+                }
+                // ...Otra asignación
+            ],
+        },
+        {
+            nombreClase: "POO",
+            codigo: "IS-400",
+            seccion: "1500",
+            descripcion: "Clase donde se enseña a programar",
+            participantes: [{
+                    nombre: "Krillin",
+                    correo: "krillin@gmail.com",
+                    imagen: "krillin.jpg",
+                },
+                {
+                    nombre: "Uub",
+                    correo: "Uub@gmail.com",
+                    imagen: "Uub.jpg"
+                },
+                {
+                    nombre: "Videl",
+                    correo: "Videl@gmail.com",
+                    imagen: "videl.jpg"
+                },
+                {
+                    nombre: "Bulma",
+                    correo: "bulma@gmail.com",
+                    imagen: "bulma.jpg"
+                }
+
+                // ...Otro participante
+            ],
+            anuncios: [{
+                    mensaje: "Hoy es el examen",
+                    fecha: "27/02/2020",
+                },
+                {
+                    mensaje: "Manana entrega de proyecto",
+                    fecha: "28/02/2021"
+                },
+                {
+                    mensaje: "Lunes ver wandavision",
+                    fecha: "01/03/2021"
+                }
+                // ...Otro anuncio
+            ],
+            asignaciones: [{
+                    titulo: "Tarea 4",
+                    descripcion: "Entregar un programa",
+                    fecha: "28/02/2021",
+                },
+                {
+                    titulo: "Tarea 5",
+                    descripcion: "Entregar un examen",
+                    fecha: "27/02/2020"
+                },
+                {
+                    titulo: "Tarea 6",
+                    descripcion: "Entregar proyecto",
+                    fecha: "28/02/2020"
+                }
+                // ...Otra asignación
+            ],
+        },
+        {
+            nombreClase: "Programacion Web",
+            codigo: "IS-900",
+            seccion: "1700",
+            descripcion: "Clase donde se enseña a programar",
+            participantes: [{
+                    nombre: "Krillin",
+                    correo: "krillin@gmail.com",
+                    imagen: "krillin.jpg",
+                },
+                {
+                    nombre: "Uub",
+                    correo: "Uub@gmail.com",
+                    imagen: "Uub.jpg"
+                },
+                {
+                    nombre: "Videl",
+                    correo: "Videl@gmail.com",
+                    imagen: "videl.jpg"
+                },
+                {
+                    nombre: "Bulma",
+                    correo: "bulma@gmail.com",
+                    imagen: "bulma.jpg"
+                }
+
+                // ...Otro participante
+            ],
+            anuncios: [{
+                    mensaje: "Hoy es el examen",
+                    fecha: "27/02/2020",
+                },
+                {
+                    mensaje: "Manana entrega de proyecto",
+                    fecha: "28/02/2021"
+                },
+                {
+                    mensaje: "Lunes ver wandavision",
+                    fecha: "01/03/2021"
+                }
+                // ...Otro anuncio
+            ],
+            asignaciones: [{
+                    titulo: "Tarea 7",
+                    descripcion: "Entregar un programa",
+                    fecha: "28/02/2022",
+                },
+                {
+                    titulo: "Tarea 8",
+                    descripcion: "Entregar un examen",
+                    fecha: "27/02/2022"
+                },
+                {
+                    titulo: "Tarea 9",
+                    descripcion: "Entregar proyecto",
+                    fecha: "28/02/2022"
+                }
+                // ...Otra asignación
+            ],
+        },
+        //...Otra clase
+    ],
+}, {
+
+
+    instructor: {
+        nombre: "Vegeta",
+        correo: "vegeta@gmail.com",
+        imagen: "vegeta.jpg",
+    },
+    clases: [{
+            nombreClase: "Programación II",
+            codigo: "CSD33XZ",
+            seccion: "1300",
+            descripcion: "Clase donde se enseña a programar",
+            participantes: [{
+                    nombre: "Krillin",
+                    correo: "krillin@gmail.com",
+                    imagen: "krillin.jpg",
+                },
+                {
+                    nombre: "Uub",
+                    correo: "Uub@gmail.com",
+                    imagen: "Uub.jpg"
+                },
+                {
+                    nombre: "Videl",
+                    correo: "Videl@gmail.com",
+                    imagen: "videl.jpg"
+                },
+                {
+                    nombre: "Bulma",
+                    correo: "bulma@gmail.com",
+                    imagen: "bulma.jpg"
+                }
+
+                // ...Otro participante
+            ],
+            anuncios: [{
+                    mensaje: "Hoy es el examen",
+                    fecha: "27/02/2020",
+                },
+                {
+                    mensaje: "Manana entrega de proyecto",
+                    fecha: "28/02/2021"
+                },
+                {
+                    mensaje: "Lunes ver wandavision",
+                    fecha: "01/03/2021"
+                }
+                // ...Otro anuncio
+            ],
+            asignaciones: [{
+                    titulo: "Tarea 1",
+                    descripcion: "Entregar un programa",
+                    fecha: "28/02/2020",
+                },
+                {
+                    titulo: "Tarea 2",
+                    descripcion: "Entregar un examen",
+                    fecha: "27/02/2021"
+                },
+                {
+                    titulo: "Tarea 3",
+                    descripcion: "Entregar proyecto",
+                    fecha: "28/02/2021"
+                }
+                // ...Otra asignación
+            ],
+        },
+        {
+            nombreClase: "POO",
+            codigo: "IS-400",
+            seccion: "1500",
+            descripcion: "Clase donde se enseña a programar",
+            participantes: [{
+                    nombre: "Krillin",
+                    correo: "krillin@gmail.com",
+                    imagen: "krillin.jpg",
+                },
+                {
+                    nombre: "Uub",
+                    correo: "Uub@gmail.com",
+                    imagen: "Uub.jpg"
+                },
+                {
+                    nombre: "Videl",
+                    correo: "Videl@gmail.com",
+                    imagen: "videl.jpg"
+                },
+                {
+                    nombre: "Bulma",
+                    correo: "bulma@gmail.com",
+                    imagen: "bulma.jpg"
+                }
+
+                // ...Otro participante
+            ],
+            anuncios: [{
+                    mensaje: "Hoy es el examen",
+                    fecha: "27/02/2020",
+                },
+                {
+                    mensaje: "Manana entrega de proyecto",
+                    fecha: "28/02/2021"
+                },
+                {
+                    mensaje: "Lunes ver wandavision",
+                    fecha: "01/03/2021"
+                }
+                // ...Otro anuncio
+            ],
+            asignaciones: [{
+                    titulo: "Tarea 4",
+                    descripcion: "Entregar un programa",
+                    fecha: "28/02/2021",
+                },
+                {
+                    titulo: "Tarea 5",
+                    descripcion: "Entregar un examen",
+                    fecha: "27/02/2020"
+                },
+                {
+                    titulo: "Tarea 6",
+                    descripcion: "Entregar proyecto",
+                    fecha: "28/02/2020"
+                }
+                // ...Otra asignación
+            ],
+        },
+        {
+            nombreClase: "Programacion Web",
+            codigo: "IS-900",
+            seccion: "1700",
+            descripcion: "Clase donde se enseña a programar",
+            participantes: [{
+                    nombre: "Krillin",
+                    correo: "krillin@gmail.com",
+                    imagen: "krillin.jpg",
+                },
+                {
+                    nombre: "Uub",
+                    correo: "Uub@gmail.com",
+                    imagen: "Uub.jpg"
+                },
+                {
+                    nombre: "Videl",
+                    correo: "Videl@gmail.com",
+                    imagen: "videl.jpg"
+                },
+                {
+                    nombre: "Bulma",
+                    correo: "bulma@gmail.com",
+                    imagen: "bulma.jpg"
+                }
+
+                // ...Otro participante
+            ],
+            anuncios: [{
+                    mensaje: "Hoy es el examen",
+                    fecha: "27/02/2020",
+                },
+                {
+                    mensaje: "Manana entrega de proyecto",
+                    fecha: "28/02/2021"
+                },
+                {
+                    mensaje: "Lunes ver wandavision",
+                    fecha: "01/03/2021"
+                }
+                // ...Otro anuncio
+            ],
+            asignaciones: [{
+                    titulo: "Tarea 7",
+                    descripcion: "Entregar un programa",
+                    fecha: "28/02/2022",
+                },
+                {
+                    titulo: "Tarea 8",
+                    descripcion: "Entregar un examen",
+                    fecha: "27/02/2022"
+                },
+                {
+                    titulo: "Tarea 9",
+                    descripcion: "Entregar proyecto",
+                    fecha: "28/02/2022"
+                }
+                // ...Otra asignación
+            ],
+        },
+        //...Otra clase
+    ],
+}];
+
+// Usando el localStorage
+
+var localStorage = window.localStorage;
+localStorage.clear
+for (i = 0; i < classroom.length; i++) {
+    localStorage.setItem('classroom', JSON.stringify(classroom));
+
+};
+
+let aula = JSON.parse(localStorage.getItem('classroom'));
+console.log('Estos es lo que devuele', aula);
